884830498f91ec108ef085460e0a6f33426bdac5
Gosia Magdzik <melassa26@gmail.com>
2023-12-14 21:32:36 +0100

    timer text styling

--- src/components/Timer/ProgressCircle/Clock/styled.js
+++ src/components/Timer/ProgressCircle/Clock/styled.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
 export const ClockContainer = styled.div`
-
+    display: grid;
+    place-items: center;
+    flex-direction: column;
 `
 
 export const TimerText = styled.h3`
     color: ${({ theme }) => theme.color.Periwinkle};
     font-size: 10rem;
+    margin: 0;
+`
+
+export const StartPauseButton = styled.button`
+    text-align: center;
+    all: unset;
+    color: ${({ theme }) => theme.color.Periwinkle};
+    font-size: 3rem;
+    letter-spacing: 1rem;
 `
